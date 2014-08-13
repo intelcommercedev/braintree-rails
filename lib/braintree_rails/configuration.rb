567e5cafee3e1da1730ca58f9874ace643ff37b7
elja <elja1989@gmail.com>
2014-08-13 17:56:30 +0300

    added configuration for cvv

--- lib/braintree_rails/configuration.rb
+++ lib/braintree_rails/configuration.rb
@@ -10,12 +10,14 @@ module BraintreeRails
     singleton_class.class_eval do
       delegate :custom_user_agent, :environment, :merchant_id, :public_key, :private_key, :logger, :to => Braintree::Configuration
       delegate :custom_user_agent=, :environment=, :merchant_id=, :public_key=, :private_key=, :logger=, :to => Braintree::Configuration
-      attr_accessor :mode, :require_postal_code, :require_street_address, :client_side_encryption_key, :default_merchant_account_id
+      attr_accessor :mode, :require_postal_code, :require_cvv, :require_street_address, :client_side_encryption_key, :default_merchant_account_id
     end
 
     self.custom_user_agent = "braintree-rails-#{Version}"
+
     self.mode = Mode::JS
     self.require_postal_code = true
     self.require_street_address = true
+    self.require_postal_code = true
   end
 end
